669d5a2135ac250497b691116ad2ddfdc2ea8b98
langtu262 <trungkienit87@gmail.com>
2024-05-05 21:37:41 +0700

    1

--- Assets/MyGame/Script/Player.cs
+++ Assets/MyGame/Script/Player.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 namespace KienIT.Gameplay
 {
     
     public class Player : MonoBehaviour
 
     {
+        
+        [SerializeField] float TimeAtack;
+        private bool attacking = false;
         private Animator r_Anim;
+        private int idAttacking;
+        private float currenAttack;
+
 
         private void Awake()
         {
             r_Anim = GetComponent<Animator>();
+            idAttacking = Animator.StringToHash("Attacking");
         }
         void Start()
         {
@@ -21,9 +29,25 @@ namespace KienIT.Gameplay
         // Update is called once per frame
         void Update()
         {
-            Input.GetMouseButtonDown(0);
-            {
+            Debug.Log(currenAttack);
 
+            if (Input.GetMouseButtonDown(0) && r_Anim != null && currenAttack <= 0)
+            {
+                r_Anim.SetBool(idAttacking, true);
+                attacking = true;
+                currenAttack = TimeAtack;
+            }
+            
+            if (currenAttack > 0)
+            {
+               currenAttack = currenAttack - Time.deltaTime;
+            }
+        }
+        public void ResetAttack()
+        {
+            if(r_Anim != null)
+            {
+                r_Anim.SetBool(idAttacking, false);
             }
         }
     }
