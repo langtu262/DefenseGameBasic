efdf9ef193fa270cf2fc143b5fe0b815d08a41ad
langtu262 <trungkienit87@gmail.com>
2024-05-14 16:18:02 +0700

    1

--- Assets/MyGame/Script/Enemy.cs
+++ Assets/MyGame/Script/Enemy.cs
@@ -6,18 +6,23 @@ namespace KienIT.Gameplay
 
     public class Enemy : MonoBehaviour
     {
-        [SerializeField] private Rigidbody2D speedMove;
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private float speedMove;
         private Player player;
         void Start()
         {
-            speedMove = GetComponent<Rigidbody2D>();
+            rb = GetComponent<Rigidbody2D>();
             player = FindObjectOfType<Player>();
         }
 
         // Update is called once per frame
         void Update()
         {
-       
+           // if (player != null)
+            //{
+               // Debug.Log("da tim thay");
+                rb.velocity = new Vector2(-speedMove, rb.position.y);
+           // }
         }
     }
 }
